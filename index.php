87f15306c6bb7ae684da9b64c7500e10e24fb74d
ELI <ecra714@gmail.com>
2017-07-03 12:03:52 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php include('header.php');?>
+
+<section id="datos">
+
+<div class="container">
+<div class="row">
+  <div class="col-lg-12 text-center">
+    <h2 class="section-heading">Datos</h2>
+  </div>
+</div>
+</div>
+
+<div class="container">
+<div class="row">
+<!--\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/-->
+<div class="col-lg-3 col-md-6 text-center">
+  <div class="service-box">
+  <a href="page-acredita.php">
+  <i class="fa fa-4x fa-check-circle-o text-primary sr-icons"></i>
+  <h3>Acreditaciones</h3>
+  <p>Quieres saber si la institución a la que quieres optar esta acreditada?
+    Pues aquí podrás encontrar la información de cada institución.
+</p>
+  </a>
+  </div>
+</div>
+<!--\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/-->
+<div class="col-lg-3 col-md-6 text-center">
+  <div class="service-box">
+  <a href="page-ubica.php">
+  <i class="fa fa-4x fa-building-o text-primary sr-icons"></i>
+  <h3>Ubicaciones</h3>
+  <p>Información especifica del lugar y contacto  de cada institución.</p>
+  </a>
+  </div>
+</div>
+<!--\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/-->
+<div class="col-lg-3 col-md-6 text-center">
+  <div class="service-box">
+  <a href="page-arancel.php">
+  <i class="fa fa-4x fa-money text-primary sr-icons"></i>
+  <h3>Aranceles</h3>
+  <p>Encuentra aquí, la información del valor del arancel de cada institución para la carrera de Diseño.</p>
+  </a>
+  </div>
+</div>
+<!--\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/-->
+<div class="col-lg-3 col-md-6 text-center">
+  <div class="service-box">
+  <a href="page-proyecta.php">
+  <i class="fa fa-4x fa-commenting-o text-primary sr-icons"></i>
+  <h3>Proyecciones</h3>
+  <p>Cuál será tu mención o cuanto ganarás? Toda la información Aquí. </p>
+  </a>
+  </div>
+</div>
+<!--\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/-->
+</div>
+</div>
+</section>
+
+<?php include('viz.php');?>
+<?php include('contact.php');?>
+<?php include('footer.php');?>
---
+++ viz.php
@@ -0,0 +1,356 @@
+<section class="no-padding" id="viz">
+
+<div class="container">
+<div class="row">
+<div class="col-lg-12 text-center">
+<hr>
+<h2>Visualizaciones</h2>
+</div>
+</div>
+</div>
+
+<div class="container-fluid">
+<div class="row no-gutter popup-gallery">
+
+  <!--\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/-->
+
+  <div class="col-lg-4 col-sm-6">
+  <a href="#portfolioModal1"  class="portfolio-box" data-toggle="modal">
+  <img src="img/foto-1.jpg" class="img-responsive" alt="">
+  <div class="portfolio-box-caption">
+  <div class="portfolio-box-caption-content">
+  <div class="project-category text-faded">
+  Carrera de Diseño
+  </div>
+  <div class="project-name">
+Valor de Arancel y Título
+  </div>
+  </div>
+  </div>
+  </a>
+  </div>
+
+  <!--\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/-->
+
+  <div class="col-lg-4 col-sm-6">
+  <a href="#portfolioModal2"  class="portfolio-box" data-toggle="modal">
+  <img src="img/foto-2.jpg" class="img-responsive" alt="">
+  <div class="portfolio-box-caption">
+  <div class="portfolio-box-caption-content">
+  <div class="project-category text-faded">
+Empleabilidad Diseño
+  </div>
+  <div class="project-name">
+Valor porcentual de las 10 principales Universidades del país
+  </div>
+  </div>
+  </div>
+  </a>
+  </div>
+
+  <!--\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/-->
+
+  <div class="col-lg-4 col-sm-6">
+  <a href="#portfolioModal3"  class="portfolio-box" data-toggle="modal">
+  <img src="img/foto-3.jpg" class="img-responsive" alt="">
+  <div class="portfolio-box-caption">
+  <div class="portfolio-box-caption-content">
+  <div class="project-category text-faded">
+  Indicadores porcentuales
+  </div>
+  <div class="project-name">
+Índice de deserción del primer año de carrera de Diseño
+  </div>
+  </div>
+  </div>
+  </a>
+  </div>
+
+  <!--\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/-->
+
+  <div class="col-lg-4 col-sm-6">
+  <a href="#portfolioModal4"  class="portfolio-box" data-toggle="modal">
+  <img src="img/foto-1.jpg" class="img-responsive" alt="">
+  <div class="portfolio-box-caption">
+  <div class="portfolio-box-caption-content">
+  <div class="project-category text-faded">
+   Índice de Procedencia educativa
+</div>
+  <div class="project-name">
+Indicadores de procedencia de establecimientos educativos
+  </div>
+  </div>
+  </div>
+  </a>
+  </div>
+
+  <!--\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/-->
+
+  <div class="col-lg-4 col-sm-6">
+  <a href="#portfolioModal5"  class="portfolio-box" data-toggle="modal">
+  <img src="img/foto-2.jpg" class="img-responsive" alt="">
+  <div class="portfolio-box-caption">
+  <div class="portfolio-box-caption-content">
+  <div class="project-category text-faded">
+Puntajes de selección de Estudiantes Unversidad de Chile
+  </div>
+  <div class="project-name">
+Máximo y Minímo ponderado
+  </div>
+  </div>
+  </div>
+  </a>
+  </div>
+
+  <!--\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/-->
+
+  <div class="col-lg-4 col-sm-6">
+  <a href="#portfolioModal6"  class="portfolio-box" data-toggle="modal">
+  <img src="img/foto-3.jpg" class="img-responsive" alt="">
+  <div class="portfolio-box-caption">
+  <div class="portfolio-box-caption-content">
+  <div class="project-category text-faded">
+   Edad, género y procedencia regional
+  </div>
+  <div class="project-name">
+Estudiantes que ingresan al primer año de la carrera de Diseño de la Universidad de Chile.
+  </div>
+  </div>
+  </div>
+  </a>
+  </div>
+
+  <!--\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/-->
+
+</div>
+</div>
+</section>
+
+<!--\/\/\/\/\/\/\/\/\/\/\/\ M O D A L E S \/\/\/\/\/\/\/\/\/\/\/\-->
+
+<div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal-content">
+<div class="close-modal" data-dismiss="modal">
+<div class="lr">
+<div class="rl">
+</div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col-lg-8 col-lg-offset-2">
+<div class="modal-body">
+<h2>Valor de Arancel y Título</h2>
+<hr class="star-primary">
+<img src="img/datos-1.png" class="img-responsive img-centered" alt="">
+<p><a href="http://www.datavizcatalogue.com/methods/parallel_sets.html">Alluvial Diagram</a>nos ayuda a identificar categorías y su correlación con diferentes elementos, en este caso relaciona el costo de arancel y el costo del valor de título de la carrera de Diseño en 10 establecimientos educativos, tomando como preferencia en el grosor de linea este ultimo. Para mayor información puedes visitar el sitio web
+  <a href="http://www.mifuturo.cl/index.php/donde-y-que-estudiar/buscador-de-carreras?tecnico=false&cmbregiones=0&cmbareas=3&cmbcanombres=224&cmbinnombres=0&limitstart=0">mifuturo.cl</a></p>
+<p>Los datos agregados en RAW para la elaboración del gráfico son:</p>
+Paso 1(Ordenar los datos):
+<img src="img/dato-1-1.jpg" class="img-responsive img-centered" alt="">
+Paso 2(selección de gráfico):
+<img src="img/dato-1-2.jpg" class="img-responsive img-centered" alt="">
+Paso 3(Ordenar los datos para la visualización):
+<img src="img/dato-1-3.jpg" class="img-responsive img-centered" alt="">
+Paso 4(Descargar la imagen):
+<img src="img/dato-1-4.jpg" class="img-responsive img-centered" alt="">
+<button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<!--\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/-->
+
+<div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal-content">
+<div class="close-modal" data-dismiss="modal">
+<div class="lr">
+<div class="rl">
+</div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col-lg-8 col-lg-offset-2">
+<div class="modal-body">
+<h2>Porcentaje de Empleabilidad</h2>
+<hr class="star-primary">
+<img src="img/datos-2.png" class="img-responsive img-centered" alt="">
+<p>Con este gráficopodemos visualizar a través del tamaño de cada recuadro, el porcentaje de empleabilidad de la carrera de Diseño, según los datos obtenidos de las 10 principales universidades del país. Para mayor información puedes visitar el sitio web <a href="http://www.mifuturo.cl/index.php/futuro-laboral/buscador-por-carrera-d-institucion?tecnico=false&cmbtipos=3&cmbinstituciones=0&cmbcarreras=21">mifuturo.cl</a>. donde se encuentra en detalle la información.</p>
+<p>Treemap, es un diagrama utilizado para grandes cantidades de datos que se pueden estructurar de forma jerárquica cuadrada o rectangular <a href="http://www.datavizcatalogue.com/methods/treemap.html">datavizcatalogue</a>. En esta página encontrarás toda la info acerca del gráfico </p>
+
+<p>Los datos agregados en RAW para la elaboración del gráfico son:</p>
+Paso 1(Ordenar los datos):
+<img src="img/dato-2-1.jpg" class="img-responsive img-centered" alt="">
+Paso 2(selección de gráfico):
+<img src="img/dato-2-2.jpg" class="img-responsive img-centered" alt="">
+Paso 3(Ordenar los datos para la visualización):
+<img src="img/dato-2-3.jpg" class="img-responsive img-centered" alt="">
+Paso 4(Descargar la imagen):
+<img src="img/dato-2-4.jpg" class="img-responsive img-centered" alt="">
+<button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<!--\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/-->
+
+<div class="portfolio-modal modal fade" id="portfolioModal3" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal-content">
+<div class="close-modal" data-dismiss="modal">
+<div class="lr">
+<div class="rl">
+</div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col-lg-8 col-lg-offset-2">
+<div class="modal-body">
+<h2>Índice de Deserción en Diseño</h2>
+<hr class="star-primary">
+<img src="img/datos-3.png" class="img-responsive img-centered" alt="">
+<p>Con la ayuda de este diagrama nos damos cuenta cuales son los indicadores de deserción del primer año de carrera de Diseño en establecimientos educativos técnico y superior, ayudandonos a reflexionar sobre el por qué, posiblemente no se entregue una educación de calidad, dejando de lado los problemas personales.Para mayor información puedes visitar el sitio web<a href="https://sellfy.com/p/8Q9P/jV3VZ/">Flat Icons</a>. donde se encuentra en detalle la información.</p>
+<p>Treemap, es un diagrama utilizado para grandes cantidades de datos que se pueden estructurar de forma jerárquica cuadrada o rectangular <a href="http://www.datavizcatalogue.com/methods/treemap.html">datavizcatalogue</a>. En esta página encontrarás toda la info acerca del gráfico.</p>
+
+
+<p>Los datos agregados en RAW para la elaboración del gráfico son:</p>
+Paso 1(Ordenar los datos):
+<img src="img/dato-3-1.jpg" class="img-responsive img-centered" alt="">
+Paso 2(selección de gráfico):
+<img src="img/dato-3-2.jpg" class="img-responsive img-centered" alt="">
+Paso 3(Ordenar los datos para la visualización):
+<img src="img/dato-3-3.jpg" class="img-responsive img-centered" alt="">
+Paso 4(Descargar la imagen):
+<img src="img/dato-3-4.jpg" class="img-responsive img-centered" alt="">
+<button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<!--\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/-->
+
+<div class="portfolio-modal modal fade" id="portfolioModal4" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal-content">
+<div class="close-modal" data-dismiss="modal">
+<div class="lr">
+<div class="rl">
+</div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col-lg-8 col-lg-offset-2">
+<div class="modal-body">
+<h2>Indicadores de procedencia de establecimientos educativos</h2>
+<hr class="star-primary">
+<img src="img/datos-4.png" class="img-responsive img-centered" alt="">
+<p>En este diagrama de torta se presenta la  procedencia educativa de los estudiantes que ingresan al primer año de la carrera de Diseño de la Universidad de Chile. Para mayor información puedes visitar el sitio web <a href="http://dis.uchilefau.cl/datos-duros/">dis.uchile.fau.cl</a> donde se encuentra en detalle la información.</p>
+Los gráficos de torta ayudan a mostrar proporciones y porcentajes entre categorías, dividiendo un círculo en segmentos proporcionales. Cada longitud de arco representa una proporción de cada categoría, mientras que el círculo completo representa la suma total de todos los datos, igual al 100%.<a href="http://www.datavizcatalogue.com/methods/pie_chart.html">datavizcatalogue</a></p>
+
+</div>
+<p>Los datos agregados en RAW para la elaboración del gráfico son:</p>
+Paso 1(Ordenar los datos):
+<img src="img/dato-4-1.jpg" class="img-responsive img-centered" alt="">
+Paso 2(selección de gráfico):
+<img src="img/dato-4-2.jpg" class="img-responsive img-centered" alt="">
+Paso 3(Ordenar los datos para la visualización):
+<img src="img/dato-4-3.jpg" class="img-responsive img-centered" alt="">
+Paso 4(Descargar la imagen):
+<img src="img/dato-4-4.jpg" class="img-responsive img-centered" alt="">
+
+<button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<!--\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/-->
+
+<div class="portfolio-modal modal fade" id="portfolioModal5" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal-content">
+<div class="close-modal" data-dismiss="modal">
+<div class="lr">
+<div class="rl">
+</div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col-lg-8 col-lg-offset-2">
+<div class="modal-body">
+<h2>Puntajes de selección de Estudiantes</h2>
+<hr class="star-primary">
+<img src="img/datos-5.png" class="img-responsive img-centered" alt="">
+<p>En este diagrama de torta, nos damos cuenta de los indicadores de rango de los puntajes de selecciónU de aquellos estudiantes  que ingresan al primer año de la carrera de Diseño de la Universidad de Chile. Para mayor información puedes visitar el sitio web<a href="http://dis.uchilefau.cl/datos-duros/">dis.uchile.fau.cl</a>. donde se encuentra en detalle la información.</a></p>
+Los gráficos de torta ayudan a mostrar proporciones y porcentajes entre categorías, dividiendo un círculo en segmentos proporcionales. Cada longitud de arco representa una proporción de cada categoría, mientras que el círculo completo representa la suma total de todos los datos, igual al 100%.<a href="http://www.datavizcatalogue.com/methods/pie_chart.html">datavizcatalogue</a></p>
+<p>Los datos agregados en RAW para la elaboración del gráfico son:</p>
+Paso 1(Ordenar los datos):
+<img src="img/dato-5-1.jpg" class="img-responsive img-centered" alt="">
+Paso 2(selección de gráfico):
+<img src="img/dato-5-2.jpg" class="img-responsive img-centered" alt="">
+Paso 3(Ordenar los datos para la visualización):
+<img src="img/dato-5-3.jpg" class="img-responsive img-centered" alt="">
+Paso 4(Descargar la imagen):
+<img src="img/dato-5-4.jpg" class="img-responsive img-centered" alt="">
+
+<button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<!--\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/-->
+
+<div class="portfolio-modal modal fade" id="portfolioModal6" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal-content">
+<div class="close-modal" data-dismiss="modal">
+<div class="lr">
+<div class="rl">
+</div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col-lg-8 col-lg-offset-2">
+<div class="modal-body">
+<h2>Edad, género y procedencia regional</h2>
+<hr class="star-primary">
+<img src="img/datos-6.png" class="img-responsive img-centered" alt="">
+
+<p>Diagrama de torta que nos muestra la diferencia entre los datos obtenidos de Edad, género y procedencia regional de los estudiantes que ingresan al primer año de la carrera de Diseño de la Universidad de Chile. Para mayor información puedes visitar el sitio web <a href="http://dis.uchilefau.cl/datos-duros/">dis.uchile.fau.cl</a>. donde se encuentra en detalle la información.</a></p>
+Los gráficos de torta ayudan a mostrar proporciones y porcentajes entre categorías, dividiendo un círculo en segmentos proporcionales. Cada longitud de arco representa una proporción de cada categoría, mientras que el círculo completo representa la suma total de todos los datos, igual al 100%.<a href="http://www.datavizcatalogue.com/methods/pie_chart.html">datavizcatalogue</a></p>
+
+<p>Los datos agregados en RAW para la elaboración del gráfico son:</p>
+Paso 1(Ordenar los datos):
+<img src="img/dato-6-1.jpg" class="img-responsive img-centered" alt="">
+Paso 2(Selección de gráfico):
+<img src="img/dato-6-2.jpg" class="img-responsive img-centered" alt="">
+Paso 3(Ordenar los datos para la visualización):
+<img src="img/dato-6-3.jpg" class="img-responsive img-centered" alt="">
+Paso 4(Descargar la imagen):
+<img src="img/dato-6-4.jpg" class="img-responsive img-centered" alt="">
+<button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<!--\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/-->
